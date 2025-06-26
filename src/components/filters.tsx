ddd2c599a5f14a4d7f668d9eea15f24691588ab5
GermanDevF <alejandrogermanleon@gmail.com>
2025-06-25 23:42:00 -0700

    feat: add new feature

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -1,11 +1,15 @@
+import { Suspense } from "react";
+
 import { AccountFilter } from "@/components/account-filter";
 import { DateFilter } from "@/components/date-filter";
 
 export const Filters = () => {
   return (
     <div className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2">
-      <AccountFilter />
-      <DateFilter />
+      <Suspense fallback={<div>Loading...</div>}>
+        <AccountFilter />
+        <DateFilter />
+      </Suspense>
     </div>
   );
 };
