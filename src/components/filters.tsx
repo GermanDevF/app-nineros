7db913c1c2f05c929f2a0bfaacc604c32da148ec
GermanDevF <alejandrogermanleon@gmail.com>
2025-06-30 19:40:20 -0700

    update components

--- src/components/filters.tsx
+++ src/components/filters.tsx
@@ -1,12 +1,21 @@
 import { Suspense } from "react";
 
+import { FiltersSk } from "@/components/skelletons/filters-sk";
+
 import { AccountFilter } from "@/components/account-filter";
 import { DateFilter } from "@/components/date-filter";
 
-export const Filters = () => {
+export const Filters = async () => {
   return (
     <div className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2">
-      <Suspense fallback={<div>Loading...</div>}>
+      <Suspense
+        fallback={
+          <div className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2">
+            <FiltersSk />
+            <FiltersSk />
+          </div>
+        }
+      >
         <AccountFilter />
         <DateFilter />
       </Suspense>
---
+++ src/components/skelletons/filters-sk.tsx
@@ -0,0 +1,12 @@
+export function FiltersSk() {
+  return (
+    <div className="flex flex-col lg:flex-row items-center gap-y-2 lg:gap-y-0 lg:gap-x-2">
+      <div className="w-full lg:w-1/3">
+        <div className="h-10 bg-gray-200 rounded-md animate-pulse"></div>
+      </div>
+      <div className="w-full lg:w-1/3">
+        <div className="h-10 bg-gray-200 rounded-md animate-pulse"></div>
+      </div>
+    </div>
+  );
+}
